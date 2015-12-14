1fe417676b086e62b449b740075141ae9c889879
Rainer Sieger <rsieger@pangaea.de>
2015-12-14 08:27:30 +0100

    show Progress Bar?

--- trunk/Headers/Application.h
+++ trunk/Headers/Application.h
@@ -71,6 +71,8 @@ public:
     QString     gs_Version;                      //!< Aktuelle Version des Programms.
     QString     gs_lastDate;                     //!< Datum des letzten Downloads
 
+    bool        gb_showProgressBar;              //!< Soll der ProgressBar im Fenster unten rechts angezeigt werden?
+
     QString     gs_ExportFilename;
     QString     gs_ShipName;
     QString     gs_CruiseLabel;
@@ -153,7 +155,7 @@ private:
     void clearMessage();
     void createActions();
     void createMenus();
-    void createStatusBar();
+    void createStatusBar( const bool showProgressBar );
     void endTool( const int err, const int stopProgress, int &ActionNumber, const QString &FilenameFormat, const int Extension, QStringList &FilenameList, const QString &doneMessage = "Done", const QString &canceledMessage = "Converter was canceled", const bool clearList = false, const bool incActionNumber = true );
     void initFileProgress( const int NumOfFiles, const QString &FilenameIn, const QString &MessageText );
     void initProgress( const int NumOfFiles, const QString &Filename, const QString &MessageText, const int totalNumberOfSteps );
