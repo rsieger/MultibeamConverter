851896e472341f9106200fea794948f0b8223faf
Rainer Sieger <rsieger@pangaea.de>
2017-06-04 12:00:27 +0200

    Qt 5.8.0 -> Qt 5.9.0

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,10 +7,10 @@ echo - macdeployqt
 cd ~/Development/GitHub/MultibeamConverter
 
 rm -R '../../Distribution/MultibeamConverter/MultibeamConverter.app'
-cp -R './build-MultibeamConverter-Desktop_Qt_5_8_0_clang_64bit-Release/MultibeamConverter.app' '../../Distribution/MultibeamConverter/MultibeamConverter.app'
+cp -R './build-MultibeamConverter-Desktop_Qt_5_9_0_clang_64bit-Release/MultibeamConverter.app' '../../Distribution/MultibeamConverter/MultibeamConverter.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/MultibeamConverter/MultibeamConverter.app/Contents/Info.plist'
 
-/Developer/Qt/5.8/clang_64/bin/macdeployqt '../../Distribution/MultibeamConverter/MultibeamConverter.app'
+/Developer/Qt/5.9/clang_64/bin/macdeployqt '../../Distribution/MultibeamConverter/MultibeamConverter.app'
 
 echo - code signing
 
